5252227b8f96d8a39844bfea13228a10b3c19df0
Srajan Naik <srajan.naik@innowise.us>
2019-12-17 19:25:21 +0530

    Add project files.

---
+++ jQueryAjaxInAsp.NETMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,122 @@
+﻿using jQueryAjaxInAsp.NETMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryAjaxInAsp.NETMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll1()
+        {
+            return View(GetAllHsnTable("0025300100"));
+        }
+
+
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using (atlasdbEntities db = new atlasdbEntities())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+
+        }
+        IEnumerable<hsn_table> GetAllHsnTable(string ACID)
+        {
+            hsn_table tab = new hsn_table();
+            List < hsn_table > tabList = new List<hsn_table>();
+            using (atlasdbEntities1 db = new atlasdbEntities1())
+            {
+                tabList = db.hsn_table.Where(x => x.product_id == ACID).ToList<hsn_table>();
+                
+                return tabList;
+            }
+
+        }
+
+
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Employee emp = new Employee();
+            if (id != 0)
+            {
+                using (atlasdbEntities db = new atlasdbEntities())
+                {
+                    emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                }
+            }
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            try
+            {
+                if (emp.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+                    string extension = Path.GetExtension(emp.ImageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    emp.ImagePath = "~/AppFiles/Images/" + fileName;
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                }
+                using (atlasdbEntities db = new atlasdbEntities())
+                {
+                    if (emp.EmployeeID == 0)
+                    {
+                        db.Employees.Add(emp);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+
+                    }
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "Submitted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (atlasdbEntities db = new atlasdbEntities())
+                {
+                    Employee emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                    db.Employees.Remove(emp);
+                    db.SaveChanges();
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
